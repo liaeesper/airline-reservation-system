b32b42d0400948b308b8de75e00baf686ed9e1b6
Zhanna Kaufman <zhakauf@gmail.com>
2017-03-22 12:29:36 -0400

    Made changes for merging

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -17,27 +17,6 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		ServerInterface resSys = new ServerInterface();
-
-		// Try to get a list of airports
-		Airports airports = resSys.PopulateAirports();
-		for (Airport airport : airports) {
-			System.out.println(airport.toString());
-		}
-		//Try to get a list of flights
-		//SearchParams nothing = new SearchParams();
-		Flights flights = resSys.GetFlights();
-		for (Flight flight : flights.getFlightList()) {
-			System.out.println("Departure airport " + flight.getDepartureAirport());
-			System.out.println("Flight Number  " + Integer.toString(flight.getFlightNumber()));
-			System.out.println("Arrival airport " + flight.getArrivalAirport());
-			System.out.println("Plane type " + flight.getPlaneType());
-			System.out.println("Flight Time " + Integer.toString(flight.getFlightTime()));
-			System.out.println("First class price " + flight.getPriceFc().getMoney().toString());
-			System.out.println("First class seats " + Integer.toString(flight.getSeatFc()));
-			System.out.println("Coach seat price " + flight.getPriceC().getMoney().toString());
-			System.out.println("Get coach seats " + Integer.toString(flight.getSeatC()));
-			System.out.println();
-			
 		}
 		System.out.println("done");
 	}
