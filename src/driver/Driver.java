ccbc8f9e9a7a62d3b4aa169011cb417ae20de011
mes5991 <mes5991@gmail.com>
2017-03-22 12:48:52 -0400

    need to learn git

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -3,6 +3,9 @@ package driver;
 import airport.Airport;
 import airport.Airports;
 import dao.ServerInterface;
+import plans.SearchParams;
+import utils.Date;
+import utils.Time;
 
 public class Driver {
 
@@ -11,14 +14,36 @@ public class Driver {
 	 */
 	public static void main(String[] args) {
 		
-		ServerInterface resSys = new ServerInterface();
+		// Create ServerInterface object
+		ServerInterface Server = new ServerInterface();
 
-		// Try to get a list of airports
-		Airports airports = resSys.PopulateAirports();
+		// Try to get a list of airports. 
+		Airports airports = Server.PopulateAirports();
 		for (Airport airport : airports) {
-			System.out.println(airport.toString());
+			System.out.println(airport);
 		}
 		System.out.println("done");
+		
+		//Create search parameters to test flights query
+		Date departureDate = new Date(05, 05, 2017);
+		Time[] departureTime = null;
+		char[] departureAirportCode = {'B','O','S'};
+		char[] arrivalAirportCode = {'B','O','S'};
+		Date arrivalDate = null;
+		Time[] arrivalTime = null;
+		boolean isRoundTrip = true;
+		Date rDepartureDate = null;
+		Time[] rDepartureTime = null;
+		Date rArrivalDate = null;
+		Time[] rArrivalTime = null;
+		char seatType = 'N';
+		SearchParams Params = new SearchParams(departureDate, departureTime, departureAirportCode, arrivalDate, arrivalTime,
+				arrivalAirportCode, isRoundTrip, rDepartureDate, rDepartureTime, rArrivalDate, rArrivalTime, seatType);
+		
+		//Test Get departing flights
+		String xmlAirports = Server.GetDepartingFlights(Params);
+		System.out.println(xmlAirports);
+		
 	}
 	
 }
