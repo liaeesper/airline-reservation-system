ed895ac191c04c2780495bbd71f9ce2df3ed82dd
regan2 <regan.broderick.sander@gmail.com>
2017-04-17 14:42:41 -0400

    added untracked gui files

---
+++ src/gui/LoadingGui.java
@@ -0,0 +1,89 @@
+package gui;
+
+import java.awt.Button;
+import java.awt.ComponentOrientation;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SpinnerNumberModel;
+
+import plans.FlightPlan;
+import plans.FlightPlans;
+
+public class LoadingGui extends JFrame implements ActionListener, WindowListener{
+	 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// Constructor to setup GUI components and event handlers
+	public LoadingGui () {		
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0.5;
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+						
+		// loading message
+		
+		add(new JLabel("Processing Request"), gbc);  
+										
+		setTitle("Loading");  // "super" Frame sets its title
+		setSize(500, 300);        // "super" Frame sets its initial window size
+	 
+		addWindowListener(this);	 
+		setVisible(true);         // "super" Frame shows
+	}
+
+	/**
+	 * actionPerformed()
+	 * Fills out the user params object when search is clicked.
+	 * Calls handleSearch() with that object.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		System.exit(0);  // Terminate the program
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {}
+}
