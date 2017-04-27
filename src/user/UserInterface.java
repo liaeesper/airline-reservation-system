a990b0cb748597d6161b37c747373e50a86cf7cd
regan2 <regan.broderick.sander@gmail.com>
2017-04-27 13:26:18 -0400

    Removed unused functions in userInterface

--- src/user/UserInterface.java
+++ src/user/UserInterface.java
@@ -22,13 +22,9 @@ public class UserInterface {
 
 	
 	public static void main(String[] args) {
-
-		//UserInterface userInt = UserInterface.instance;
-
 		ServerInterface resSys = ServerInterface.instance;
 		Airports.instance = resSys.PopulateAirports();
 		Airplanes.instance = resSys.PopulateAirplanes();
-
 
 		UserInterface.instance.DisplaySearch();
 	}
@@ -39,38 +35,6 @@ public class UserInterface {
 	
 	public void DisplaySearchResults(ArrayList<FlightPlans> flightList, SearchParams userParams){
 		new SearchResultsGui(flightList, new ArrayList<FlightPlan>(),false, 0, userParams);
-	}
-	
-	public void DisplaySearchResultsProto(Flights flightList){
-		System.out.print("\n");
-		if(flightList.getFlightList().size() == 0){
-			System.out.print("No results :(\n");
-		}
-		int count = 1;
-		for(Flight flight: flightList.getFlightList()){
-			System.out.print(String.valueOf(count) + ".\n");
-			count++;
-			System.out.print(flight.toString());
-			System.out.print("\n");
-		}
-		
-		
-	}
-	
-	public void DisplayAlternativeSeating(FlightPlan flight){
-		
-	}
-	
-	public void DisplayNoFlights(){
-		
-	}
-	
-	public void DisplayConfirmation(Reservation reservation){
-		
-	}
-	
-	public void DisplayFinalConfirmation(){
-		
 	}
 	
 	/**
@@ -89,21 +53,5 @@ public class UserInterface {
 		
 		DisplaySearchResults(flightList, userParams);
 		return;
-	}
-	
-	public FlightPlan HandleSelection(){
-		return null;
-	}
-	
-	public void HandleConfirmation(Reservation reservation){
-		
-	}
-	
-	public void HandleSortByTime(FlightPlans flightList){
-		
-	}
-	
-	public void HandleSortByPrice(FlightPlans flightList){
-		
 	}
 }
